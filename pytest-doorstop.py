403719a861ed3de6e4adde634c358199d60d2f60
Andrew Simon <asimon@domeng.com>
2020-05-01 09:54:59 -0400

    Add TODOs

--- pytest-doorstop.py
+++ pytest-doorstop.py
@@ -27,6 +27,7 @@ def get_commit_hash() -> str:
 
 def get_document(config) -> pathlib.Path:
     """Convert commandline argument to a pathlib object."""
+    # TODO: Use he doorstop API to find this: doorstop.build().find_document(docprefix)
     doorstop_document = config.getoption("doorstop_document")
     if doorstop_document:
         return pathlib.Path(doorstop_document)
@@ -35,6 +36,8 @@ def get_document(config) -> pathlib.Path:
 
 def get_doorstop_item(nodeid: str, document: pathlib.Path) -> pathlib.Path:
     """Search for the doorstop item that contains the test."""
+    # TODO: Use the doorstop API to find this --> document.items
+    # TODO: Implement caching to avoid searching the entire tree for each test
     # TODO: Requires that the test function name be unique. Add in filename too?
     # TODO: Incorporate new array behavior
     test_name = nodeid.split("::")[-1]
@@ -47,6 +50,7 @@ def get_doorstop_item(nodeid: str, document: pathlib.Path) -> pathlib.Path:
 
 def record_outcome(doorstop_item: pathlib.Path, outcome: str, commit_hash: str, xfail: bool) -> None:
     """Write the outcome to the doorstop item."""
+    # TODO: Use the doorstop API to write this --> item.set(key, value)
     with doorstop_item.open("r") as f:
         contents = yaml.safe_load(f)
     contents["test_commit_latest"] = commit_hash
@@ -68,8 +72,11 @@ def pytest_report_teststatus(report, config) -> None:
     document = get_document(config)
     if document:
         if report.when == "call":
-            doorstop_item = get_doorstop_item(report.nodeid, document)
-            xfail = "xfail" in report.keywords
-            if doorstop_item:
+            try:
+                doorstop_item = get_doorstop_item(report.nodeid, document)
+                xfail = "xfail" in report.keywords
                 commit_hash = get_commit_hash()  # TODO: Only call this once per session
                 record_outcome(doorstop_item, report.outcome, commit_hash, xfail)
+            except RuntimeWarning:
+                # TODO: Print the warning if a verbose flag is passed
+                pass
